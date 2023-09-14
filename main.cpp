ee440641f7db28a6d3cdef4462075df47374568e
VegetableKim <114391814+VegetableKim@users.noreply.github.com>
2023-09-14 14:00:13 +0900

    2023-09-14 실습과제 #3 3번

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str1, str2;
+
+	cout << "이름을 입력하세요: ";
+	cin >> str1;
+	cout << "학번을 입력하세요: ";
+	cin >> str2;
+
+	cout << "\n<출력>\n이름: " << str1 << "\n학번: " << str2;
+}
+
